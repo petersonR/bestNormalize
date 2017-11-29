4325e352bd45bbfce63caa1d2861e79094931e87
Ryan A Peterson <ryan-peterson@uiowa.edu>
2017-11-29 10:31:21 -0600

    Fix typo in documentation

--- R/bestNormalize-package.R
+++ R/bestNormalize-package.R
@@ -1,7 +1,7 @@
 #' bestNormalize: Flexibly calculate the best normalizing transformation for a
 #' vector
 #' 
-#' The foo package provides several normalizing transformations, and introduces a
+#' The \code{bestNormalize} package provides several normalizing transformations, and introduces a
 #' new transformation based off of the order statistics, \code{orderNorm}.
 #' Perhaps the most useful function is \code{bestNormalize}, which attempts all
 #' of these transformations and picks the best one based off of a goodness of
